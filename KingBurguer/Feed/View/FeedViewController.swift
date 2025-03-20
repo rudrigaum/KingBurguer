e028ad2dee0bd4b8a566d438d27b601623723373
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-03-20 10:39:39 -0300

    add tableView

--- KingBurguer/Feed/View/FeedViewController.swift
+++ KingBurguer/Feed/View/FeedViewController.swift
@@ -8,8 +8,38 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    private let feedTableView: UITableView = {
+       let tableView = UITableView()
+        tableView.backgroundColor = .cyan
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(feedTableView)
+        feedTableView.dataSource = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        feedTableView.frame = view.bounds
+    }
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = "Olá Mundo \(indexPath.row)"
+        return cell
+    }
+    
+    
 }

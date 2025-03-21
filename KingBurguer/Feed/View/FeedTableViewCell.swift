26a489b2f97c9dbc2a41edd945039568bda2028f
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-03-21 10:16:28 -0300

    add collection view

--- KingBurguer/Feed/View/FeedTableViewCell.swift
+++ KingBurguer/Feed/View/FeedTableViewCell.swift
@@ -10,13 +10,44 @@ import UIKit
 class FeedTableViewCell: UITableViewCell {
     
     static let identifier = "FeedTableViewCell"
-
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 180)
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBlue
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.contentView.backgroundColor = .systemGreen
+        
+        contentView.addSubview(collectionView)
+        collectionView.dataSource = self
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension FeedTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 17
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.backgroundColor = .red
+        return cell
     }
 }
